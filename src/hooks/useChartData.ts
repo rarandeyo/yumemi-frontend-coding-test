e67a50196616cf48c9b1bb426d8d7482d8e10161
rarandeyo <rarandeyo@gmail.com>
2024-12-14 11:38:11 +0900

    feat: add useChartData hook

---
+++ src/hooks/useChartData.ts
@@ -0,0 +1,40 @@
+import type { PopulationLabelType } from '@/types/PopulationLabelSchema'
+import type { PopulationDataWithPrefCode } from '@/types/PopulationSchema'
+
+type ChartDataType = {
+  year: number
+  [key: number]: number
+}
+
+type UseChartDataReturn = {
+  chartData: ChartDataType[]
+}
+
+export const useChartData = (
+  selectedLabel: PopulationLabelType,
+  populationList: PopulationDataWithPrefCode[],
+): UseChartDataReturn => {
+  if (populationList.length === 0) return { chartData: [] }
+
+  const populationListFilterLabel = populationList.map((population) => ({
+    prefCode: population.prefCode,
+    data: population.data.find((data) => data.label === selectedLabel)?.data ?? [],
+  }))
+
+  const years = populationListFilterLabel[0].data.map((dataPerYear) => dataPerYear.year)
+
+  const chartData = years.map((year) => {
+    const rowData: ChartDataType = { year }
+
+    for (const populationFilterLabel of populationListFilterLabel) {
+      const prefCode = populationFilterLabel.prefCode
+      const populationValue =
+        populationFilterLabel.data.find((dataPerYear) => dataPerYear.year === year)?.value ?? 0
+      rowData[prefCode] = populationValue
+    }
+
+    return rowData
+  })
+
+  return { chartData }
+}
