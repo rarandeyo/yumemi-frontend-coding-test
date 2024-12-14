eadd1d994a8cab5ace01b20165d67914e3a65334
rarandeyo <rarandeyo@gmail.com>
2024-12-14 11:40:13 +0900

    feat: add useGetPrefectureName hook

---
+++ src/hooks/usePrefectureName.ts
@@ -0,0 +1,17 @@
+import type { PrefectureState } from '@/types/PrefecturesSchema'
+import { useCallback } from 'react'
+
+export const useGetPrefectureName = (
+  prefectureStates: PrefectureState[],
+): {
+  getPrefectureName: (prefCode: number) => string
+} => {
+  const getPrefectureName = useCallback(
+    (prefCode: number) => {
+      return prefectureStates.find((pref) => pref.prefCode === prefCode)?.prefName ?? 'undefined'
+    },
+    [prefectureStates],
+  )
+
+  return { getPrefectureName }
+}
