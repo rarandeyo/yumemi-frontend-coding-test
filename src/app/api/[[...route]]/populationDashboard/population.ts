5e11af3d0a7afe008e89e0a75ba79590467fd347
rarandeyo <rarandeyo@gmail.com>
2024-12-17 14:02:26 +0900

    fix: Do not return details of validation errors to the client

--- src/app/api/[[...route]]/populationDashboard/population.ts
+++ src/app/api/[[...route]]/populationDashboard/population.ts
@@ -9,17 +9,22 @@ const PopulationListParamsSchema = z.object({
   prefCode: z.coerce.number().pipe(PrefectureSchema.shape.prefCode),
 })
 
-const app = new Hono().get('/', zValidator('query', PopulationListParamsSchema), async (c) => {
-  try {
-    const { prefCode } = c.req.valid('query')
-    const populationData = await getPopulationData(prefCode)
-    return c.json(populationData)
-  } catch (error) {
-    if (error instanceof z.ZodError) {
+const app = new Hono().get(
+  '/',
+  zValidator('query', PopulationListParamsSchema, (result, c) => {
+    if (!result.success) {
       return c.json({ error: 'Invalid prefCode' }, 400)
     }
-    return c.json({ error: 'Internal Server Error' }, 500)
-  }
-})
+  }),
+  async (c) => {
+    try {
+      const { prefCode } = c.req.valid('query')
+      const populationData = await getPopulationData(prefCode)
+      return c.json(populationData)
+    } catch (error) {
+      return c.json({ error: 'Internal Server Error' }, 500)
+    }
+  },
+)
 
 export default app
