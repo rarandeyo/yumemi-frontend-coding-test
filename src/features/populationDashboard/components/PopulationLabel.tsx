0aae5e0967fa098d9df3d3346562252e85f5b6eb
rarandeyo <rarandeyo@gmail.com>
2024-12-23 15:29:38 +0900

    feat: memo for PopulationLabel and PrefectureCheckboxes

--- src/features/populationDashboard/components/PopulationLabel.tsx
+++ src/features/populationDashboard/components/PopulationLabel.tsx
@@ -3,32 +3,32 @@ import {
   type PopulationLabelType,
 } from '@/features/populationDashboard/types/PopulationLabelSchema'
 import type React from 'react'
+import { memo } from 'react'
 
 type SelectPopulationLabelProps = {
   selectedLabel: PopulationLabelType
   handlePopulationLabel: (e: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
-export const PopulationLabel: React.FC<SelectPopulationLabelProps> = ({
-  selectedLabel,
-  handlePopulationLabel,
-}) => {
-  return (
-    <div className="mx-auto w-full p-2 md:p-4">
-      <label className="flex justify-center">
-        <select
-          className="w-full max-w-xs rounded-md border-2 border-black bg-white px-2 py-1.5 text-sm shadow-sm focus:outline-none focus:ring-1 focus:ring-light-blue md:px-3 md:py-2 md:text-base"
-          value={selectedLabel}
-          onChange={handlePopulationLabel}
-          title="人口構成"
-        >
-          {POPULATION_LABELS.map((label) => (
-            <option key={label} value={label}>
-              {label}
-            </option>
-          ))}
-        </select>
-      </label>
-    </div>
-  )
-}
+export const PopulationLabel: React.FC<SelectPopulationLabelProps> = memo(
+  ({ selectedLabel, handlePopulationLabel }) => {
+    return (
+      <div className="mx-auto w-full p-2 md:p-4">
+        <label className="flex justify-center">
+          <select
+            className="w-full max-w-xs rounded-md border-2 border-black bg-white px-2 py-1.5 text-sm shadow-sm focus:outline-none focus:ring-1 focus:ring-light-blue md:px-3 md:py-2 md:text-base"
+            value={selectedLabel}
+            onChange={handlePopulationLabel}
+            title="人口構成"
+          >
+            {POPULATION_LABELS.map((label) => (
+              <option key={label} value={label}>
+                {label}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+    )
+  },
+)
