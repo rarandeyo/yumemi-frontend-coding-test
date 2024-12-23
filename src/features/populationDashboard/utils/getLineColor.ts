8c498169158b06ab0aaec18d0dc1103459cb1623
rarandeyo <rarandeyo@gmail.com>
2024-12-23 21:59:47 +0900

    refactor: index use prefCode -1

--- src/features/populationDashboard/utils/getLineColor.ts
+++ src/features/populationDashboard/utils/getLineColor.ts
@@ -1,5 +1,6 @@
 import { GRAPH_COLOR_PALETTE } from '@/features/populationDashboard/constants/graphColorPalette'
 
 export const getLineColor = (prefCode: number): string => {
-  return GRAPH_COLOR_PALETTE[prefCode % GRAPH_COLOR_PALETTE.length]
+  const index = (prefCode - 1) % GRAPH_COLOR_PALETTE.length
+  return GRAPH_COLOR_PALETTE[index]
 }
