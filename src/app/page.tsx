06c51f9e583f6d3b96f6e1c414be7b99b7971c89
rarandeyo <rarandeyo@gmail.com>
2024-12-27 12:41:36 +0900

    feat: 都道府県情報を仮表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>template</div>
+import type { Prefectures } from '@/types/PrefecturesSchema'
+import { getPrefectures } from '@/utils/getPrefectures'
+
+export default async function Home() {
+  const Prefectures: Prefectures = await getPrefectures()
+  return (
+    <main className="flex flex-col">
+      {Prefectures.map((pref) => (
+        <div key={pref.prefName} className="text-lg">
+          {pref.prefName}
+        </div>
+      ))}
+    </main>
+  )
 }
