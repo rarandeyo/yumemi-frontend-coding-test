1ccf9faf62032bf693cb8291373c2c35c5899fd6
rarandeyo <rarandeyo@gmail.com>
2024-12-28 04:24:20 +0900

    style: checkboxコンポーネントのUIを改善

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { getPrefectures } from '@/utils/getPrefectures'
 export default async function Home() {
   const Prefectures: Prefectures = await getPrefectures()
   return (
-    <main className="flex flex-col">
+    <main className="mx-auto flex w-full max-w-7xl flex-col space-y-2 px-4 md:space-y-4 md:px-6">
       <PrefectureCheckboxes prefectures={Prefectures} />
     </main>
   )
--- src/components/PrefectureCheckboxes.tsx
+++ src/components/PrefectureCheckboxes.tsx
@@ -12,17 +12,21 @@ export const PrefectureCheckboxes: React.FC<PrefectureCheckboxesProps> = ({ pref
   const [selectedPrefs, setSelectedPrefs] = React.useState<number[]>([])
 
   return (
-    <div>
+    <section className="mx-auto flex flex-wrap rounded-lg border border-gray-300 bg-light-blue p-4 shadow-md md:p-6 lg:p-8">
       {prefectures.map((pref) => (
-        <label key={pref.prefCode}>
-          <input
-            type="checkbox"
-            checked={selectedPrefs.includes(pref.prefCode)}
-            onChange={() => handlePrefectureCheckboxes(pref.prefCode, setSelectedPrefs)}
-          />
-          {pref.prefName}
-        </label>
+        <div key={pref.prefCode} className="w-1/2 p-1.5 md:w-1/3 lg:w-1/4 xl:w-1/5">
+          <label className="flex items-center space-x-1.5 hover:cursor-pointer md:space-x-2">
+            <input
+              className="form-checkbox h-4 w-4 text-blue-600 md:h-5 md:w-5"
+              aria-label="都道府県を選択"
+              type="checkbox"
+              checked={selectedPrefs.includes(pref.prefCode)}
+              onChange={() => handlePrefectureCheckboxes(pref.prefCode, setSelectedPrefs)}
+            />
+            <span className="text-base md:text-lg">{pref.prefName}</span>
+          </label>
+        </div>
       ))}
-    </div>
+    </section>
   )
 }
