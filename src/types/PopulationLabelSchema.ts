d18c52e62e8a0b182d0b8659eaeebf9eda814e4b
rarandeyo <rarandeyo@gmail.com>
2025-01-03 13:19:36 +0900

    rename: populationLabelSchemaをPopulationLabelSchemaに修正

--- src/types/PopulationLabelSchema.ts
+++ src/types/PopulationLabelSchema.ts
@@ -3,10 +3,10 @@ import { z } from 'zod'
 export const POPULATION_LABELS = ['総人口', '生産年齢人口', '老年人口', '年少人口'] as const
 export const POPULATION_LABELS_EN = ['TOTAL', 'WORKING', 'ELDERLY', 'YOUNG'] as const
 
-export const populationLabelSchema = z.object({
+export const PopulationLabelSchema = z.object({
   ja: z.enum(POPULATION_LABELS),
   en: z.enum(POPULATION_LABELS_EN),
 })
 
-export type JaPopulationLabelType = z.infer<typeof populationLabelSchema>['ja']
-export type EnPopulationLabelType = z.infer<typeof populationLabelSchema>['en']
+export type JaPopulationLabelType = z.infer<typeof PopulationLabelSchema>['ja']
+export type EnPopulationLabelType = z.infer<typeof PopulationLabelSchema>['en']
