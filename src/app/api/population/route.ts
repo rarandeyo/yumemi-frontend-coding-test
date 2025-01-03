9fa0c659afab0f93ea142da28e887bf55e50354b
rarandeyo <rarandeyo@gmail.com>
2025-01-03 20:08:39 +0900

    feat: 試験要件に合わせるため人口データを取得する方法をroute handlerに変更

---
+++ src/app/api/population/route.ts
@@ -0,0 +1,21 @@
+import { getPopulationData } from '@/utils/getPopulationDataList'
+import { type NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
+
+const PopulationListParamsSchema = z.number().min(1).max(47)
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const prefCode = Number(searchParams.get('prefCode'))
+
+  try {
+    const validatedPrefCode = PopulationListParamsSchema.parse(prefCode)
+    const populationList = await getPopulationData(validatedPrefCode)
+    return NextResponse.json(populationList)
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json({ error: 'Invalid prefCode' }, { status: 400 })
+    }
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+  }
+}
