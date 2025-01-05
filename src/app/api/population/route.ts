9cf48b211f50d951e0d95bc64eccd36100a816bd
rarandeyo <rarandeyo@gmail.com>
2025-01-05 11:16:05 +0900

    rename: populationListとpopulationDataに命名変更して統一

--- src/app/api/population/route.ts
+++ src/app/api/population/route.ts
@@ -3,16 +3,16 @@ import { getPopulationData } from '@/utils/getPopulationData'
 import { type NextRequest, NextResponse } from 'next/server'
 import { z } from 'zod'
 
-const PopulationListParamsSchema = PrefectureSchema.shape.prefCode
+const PopulationDataParamsSchema = PrefectureSchema.shape.prefCode
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const prefCode = Number(searchParams.get('prefCode'))
 
   try {
-    const validatedPrefCode = PopulationListParamsSchema.parse(prefCode)
-    const populationList = await getPopulationData(validatedPrefCode)
-    return NextResponse.json(populationList)
+    const validatedPrefCode = PopulationDataParamsSchema.parse(prefCode)
+    const populationData = await getPopulationData(validatedPrefCode)
+    return NextResponse.json(populationData)
   } catch (error) {
     if (error instanceof z.ZodError) {
       return NextResponse.json({ error: 'Invalid prefCode' }, { status: 400 })
